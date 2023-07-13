be5c9ef2ff9f07939111fa53dd442bc1bb322e98
Jo??o Vitor Moreira Passos <moreirapassosj@gmail.com>
2023-07-13 20:29:05 -0300

    Update redirect to signin in navbar

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "core"
+
 urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("signin/", views.signinpage, name="signin"),
