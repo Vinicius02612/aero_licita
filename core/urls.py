8ae3cf53c0d54c032a0bcd422e0a07ca9dd33d48
Jo??o Vitor Moreira Passos <moreirapassosj@gmail.com>
2023-07-16 18:11:18 -0300

    Add admin page, but still need to leave responsive.

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,7 @@ app_name = "core"
 urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("signin/", views.signinpage, name="signin"),
+    path("login/", views.loginview, name="login"),
+    path("logout/", views.logoutuser, name="logout"),
+    path("admin-management-page/", views.adminpage, name="adminpage"),
 ]
